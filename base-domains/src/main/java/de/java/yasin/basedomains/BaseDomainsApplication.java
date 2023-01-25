77ba83adbe5e91af010315e22803b0957c312d13
Yasin khorasani <yasinkhorasani@yahoo.com>
2023-01-25 10:02:06 +0100

    config server-port and create Order/OrderEvent

---
+++ base-domains/src/main/java/de/java/yasin/basedomains/BaseDomainsApplication.java
@@ -0,0 +1,13 @@
+package de.java.yasin.basedomains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDomainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDomainsApplication.class, args);
+	}
+
+}
---
+++ email-service/src/main/java/de/java/yasin/emailservice/EmailServiceApplication.java
@@ -0,0 +1,13 @@
+package de.java.yasin.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceApplication.class, args);
+	}
+
+}
